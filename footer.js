ecdf8111563524d53cbc250ce4a4d5c2878938b9
vterzoglou <vterzoglou@gmail.com>
2022-11-26 15:28:58 +0100

    Common nav&foot, slideshow hover, minor fixes

---
+++ footer.js
@@ -0,0 +1,17 @@
+document.write('<link rel="stylesheet" href="css/footer.css">\
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">\
+<footer>\
+    <hr>\
+    <div class="footer_container"> \
+        <a href="https://www.fit.vut.cz/" target="_blank"><img src="Images/VUT-FIT_logo.svg"></a>\
+        <span class="credits">\
+        Website created for FIT/ITWe<br>\
+        Vasileios Terzoglou (xterzo00)<br>\
+        Dragos Bratfalean (xbratf00)<br>\
+        </span>\
+        <a href="https://www.facebook.com/PrimariaClujNapocaRomania/" target="_blank" class="fb fa fa-facebook"></a>\
+        <a href="https://www.instagram.com/primariaclujnapoca/" target="_blank" class="inst fa fa-instagram"></a>\
+        <a href="https://primariaclujnapoca.ro/" target="_blank" class="wpage fa fa-globe"></a>\
+    </div>\
+</footer>\
+');
--- navi.js
+++ navi.js
@@ -3,11 +3,11 @@ document.write('<link rel="stylesheet" href="css/navigation_bar.css">\
     <a class="logo" href="main_page.html"><img src="images/logo.svg" alt="logo"></a>\
     <nav>\
         <ul class="navi">\
-            <li><a href="html_sections/Services.html">Services</a></li>\
-            <li><a href="html_sections/Events.html">Events</a></li>\
-            <li><a href="html_sections/History.html">History</a></li>\
-            <li><a href="html_sections/Contact.html">Contact</a></li>\
-            <li><a href="html_sections/Outdooractivities.html">Outdoor Activities</a></li>\
+            <li><a href="html_sections/Services.html" class="navi_link">Services</a></li>\
+            <li><a href="html_sections/Events.html" class="navi_link">Events</a></li>\
+            <li><a href="html_sections/History.html" class="navi_link">History</a></li>\
+            <li><a href="html_sections/Contact.html" class="navi_link">Contact</a></li>\
+            <li><a href="html_sections/Outdooractivities.html" class="navi_link">Outdoor Activities</a></li>\
         </ul>\
     </nav>\
 </header>\
