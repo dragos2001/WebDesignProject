ad4d29a152d9085072c0c5d49a675368d1684bfa
vterzoglou <vterzoglou@gmail.com>
2022-11-26 02:37:05 +0100

    Common menu(+fixes), adding base

---
+++ navi.js
@@ -0,0 +1,14 @@
+document.write('<link rel="stylesheet" href="css/navigation_bar.css">\
+<header class="mymenu">\
+    <a class="logo" href="main_page.html"><img src="images/logo.svg" alt="logo"></a>\
+    <nav>\
+        <ul class="navi">\
+            <li><a href="html_sections/Services.html">Services</a></li>\
+            <li><a href="html_sections/Events.html">Events</a></li>\
+            <li><a href="html_sections/History.html">History</a></li>\
+            <li><a href="html_sections/Contact.html">Contact</a></li>\
+            <li><a href="html_sections/Outdooractivities.html">Outdoor Activities</a></li>\
+        </ul>\
+    </nav>\
+</header>\
+');
