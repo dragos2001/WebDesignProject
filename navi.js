1983818b702a6608eb3b6c140310387f39354ef6
vterzoglou <vterzoglou@gmail.com>
2022-12-02 12:19:35 +0100

    Homepage item on navi, srvc cards non click

--- navi.js
+++ navi.js
@@ -3,6 +3,7 @@ document.write('<link rel="stylesheet" href="css/navigation_bar.css">\
     <a class="logo" href="main_page.html"><img src="images/logo.svg" alt="logo"></a>\
     <nav>\
         <ul class="navi">\
+            <li><a href="main_page.html" class="navi_link">Home Page</a></li>\
             <li><a href="html_sections/Services.html" class="navi_link">Services</a></li>\
             <li><a href="html_sections/Events.html" class="navi_link">Events</a></li>\
             <li><a href="html_sections/History.html" class="navi_link">History</a></li>\
