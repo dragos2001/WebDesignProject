d61d709797859c2aef31b38c0f34b600154c8a13
vterzoglou <vterzoglou@gmail.com>
2022-12-03 02:08:53 +0100

    Started sights page

--- navi.js
+++ navi.js
@@ -4,11 +4,11 @@ document.write('<link rel="stylesheet" href="css/navigation_bar.css">\
     <nav>\
         <ul class="navi">\
             <li><a href="main_page.html" class="navi_link">Home Page</a></li>\
-            <li><a href="html_sections/Services.html" class="navi_link">Services</a></li>\
-            <li><a href="html_sections/Events.html" class="navi_link">Events</a></li>\
             <li><a href="html_sections/History.html" class="navi_link">History</a></li>\
-            <li><a href="html_sections/Contacts.html" class="navi_link">Contact</a></li>\
+            <li><a href="html_sections/Sights.html" class="navi_link">Sights</a></li>\
+            <li><a href="html_sections/Events.html" class="navi_link">Events</a></li>\
             <li><a href="html_sections/Outdooractivities.html" class="navi_link">Outdoor Activities</a></li>\
+            <li><a href="html_sections/Services.html" class="navi_link">Services</a></li>\
         </ul>\
     </nav>\
 </header>\
